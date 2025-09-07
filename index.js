ccc52ba52683e6709b7d6df362de0fd207a691f6
developertarekrahman <tarekrahmanuiux@gmail.com>
2025-09-07 13:28:19 +0200

    Catagory api finished

---
+++ index.js
@@ -0,0 +1,31 @@
+const categoryContainer = document.getElementById("catagory-container");
+
+const loadCategory = () => {
+  fetch("https://openapi.programming-hero.com/api/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      const catagories = data.categories;
+      console.log(catagories);
+      showCategory(catagories);
+    })
+
+    .catch((err) => {
+      console.log(err);
+    });
+};
+const showCategory = (catagories) => {
+  catagories.forEach((cat) => {
+    categoryContainer.innerHTML += `
+     <li id="${cat.id}" class="hover:bg-[#15803D] hover:text-white hover:rounded-[4px] hover:px-2.5 hover:py-2 mb-4">${cat.category_name}</li>
+
+    `;
+  });
+
+  //   categoryContainer.addEventListener("click", (e) => {
+  //     const allLi = document.querySelectorAll("li");
+  //     allLi.forEach((li) => {
+  //       li.classList.remove("");
+  //     });
+};
+
+loadCategory();
